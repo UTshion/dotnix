25b64f9a4ad8f811b74c06edcd0b6b957421dcb0
UTshion <yannbarushion@gmail.com>
2025-04-25 01:38:02 +0900

    add pureref desktop entry

--- modules/home/desktop_entries.nix
+++ modules/home/desktop_entries.nix
@@ -18,5 +18,15 @@
         Keywords = "Database;SQL;IDE;JDBC;ODBC;MySQL;PostgreSQL;Oracle;DB2;MariaDB;";
       };
     };
+    pureref = {
+      name = "PureRef";
+      genericName = "Reference Image Viewer";
+      exec = "pureref %F";
+      icon = "pureref";  # アイコンファイルがある場合はパスを指定
+      comment = "Arrange and view reference images";
+      categories = [ "Graphics" "Viewer" ];
+      mimeType = [ "image/jpeg" "image/png" "image/bmp" "image/tiff" ];
+      terminal = false;
+    };
   };
 }
