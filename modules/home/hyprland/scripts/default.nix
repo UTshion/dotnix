055c8d593c7a332c3578c632078c4755da2cf1e4
UTshion <yannbarushion@gmail.com>
2025-09-09 20:54:52 +0900

    add wallpaper&theme switcher

--- modules/home/hyprland/scripts/default.nix
+++ modules/home/hyprland/scripts/default.nix
@@ -2,4 +2,5 @@
 {
   moveto = import ./moveto.nix { inherit pkgs; };
   toggleallfloat = import ./toggleallfloat.nix { inherit pkgs; };
+  change-wallpaper = import ./change-wallpaper.nix {inherit pkgs; };
 }
