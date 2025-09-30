4c74ec736247bfbe14e23aa832327d0973d0442b
UTshion <yannbarushion@gmail.com>
2025-09-30 23:54:36 +0900

    add hypridle

---
+++ modules/home/hyprland/hypridle.nix
@@ -0,0 +1,22 @@
+{
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
+      listener {
+        timeout = 150;                             
+        on-timeout = "brightnessctl -s set 10::";  
+        on-resume = "brightnessctl -r";            
+      };
+      listener { 
+        timeout = 150;                                         
+        on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0"; 
+        on-resume = "brightnessctl -rd rgb:kbd_backlight";
+      };
+    };
+  };
+};
